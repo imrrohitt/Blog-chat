57eb5a9e03c9ab4e8f50da565c1c3303b5edd72e
Rohit kushwaha <rohit.kushwaha@w3villa.com>
2024-03-08 19:49:13 +0530

    mongodb other terms removed

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const { startServer } = require('./groqService');
 
 // Connect to MongoDB (make sure to have a running MongoDB server)
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
